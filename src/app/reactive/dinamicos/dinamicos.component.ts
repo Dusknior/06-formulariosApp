3e799af28ac1edf1c56b434192356e7b3c7fd0db
Daniel Bernal Herrera <danielbernalherrera@gmail.com>
2022-04-18 17:47:02 -0500

    third commit

--- src/app/reactive/dinamicos/dinamicos.component.ts
+++ src/app/reactive/dinamicos/dinamicos.component.ts
@@ -1,16 +1,67 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import {
+  FormArray,
+  FormBuilder,
+  FormGroup,
+  Validators,
+  FormControl,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-dinamicos',
   templateUrl: './dinamicos.component.html',
-  styles: [
-  ]
+  styles: [],
 })
-export class DinamicosComponent implements OnInit {
+export class DinamicosComponent {
+  miFormulario: FormGroup = this.fb.group({
+    nombre: [, [Validators.required, Validators.minLength(3)]],
+    favoritos: this.fb.array(
+      [
+        ['Metal Gear Solid', Validators.required],
+        ['GTA', Validators.required],
+      ],
+      Validators.required
+    ),
+  });
 
-  constructor() { }
-
-  ngOnInit(): void {
+  get favoritosArr() {
+    return this.miFormulario.get('favoritos') as FormArray;
   }
 
+  constructor(private fb: FormBuilder) {}
+
+  campoValido(campo: string) {
+    return (
+      this.miFormulario.controls[campo].errors &&
+      this.miFormulario.controls[campo].touched
+    );
+  }
+
+  nuevoFavorito: FormControl = this.fb.control('', Validators.required);
+
+  agregarFavorito() {
+    if (this.nuevoFavorito.invalid) {
+      return;
+    }
+
+    this.favoritosArr.push(
+      this.fb.control(this.nuevoFavorito.value, Validators.required)
+    );
+
+    this.nuevoFavorito.reset();
+  }
+
+  borrarFavorito(i: number) {
+    this.favoritosArr.removeAt(i);
+  }
+
+  guardar = () => {
+    if (this.miFormulario.invalid) {
+      this.miFormulario.markAllAsTouched();
+      return;
+    }
+
+    console.log(this.miFormulario.value);
+    this.miFormulario.reset();
+  };
 }
